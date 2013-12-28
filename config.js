cddb67d5fb3fe4618ae94753281a5be95d51e74c
root <root@mongodb.(none)>
2013-12-28 07:24:17 +0000

    testing configuration merge using command

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ var config = {};
 config.startup = [
 	{command:"/root/local/cloud9/bin/cloud9.sh -l 0.0.0.0 -p 3333 -w /root", cwd:"/root/local/cloud9/bin", page:"cloud9", port:"3333", startup:true},
 	{command:"git fetch --all & git reset --hard origin/master", cwd:"/root/local/libra", page:"update-libra", port:"8080"},
-	{command:"cp config.js /etc/local/config.js", cwd:"/root/projects/libra", page:"update-config", port:"8080"}
+	{command:"cp config.js /etc/local/config.js", cwd:process.cwd, page:"update-config", port:"8000"}
 ];
 module.exports = config;
---
+++ sample-config.js
@@ -0,0 +1,5 @@
+var config = {};
+config.startup = [
+	{command:"cp config.js /etc/local/config.js", cwd:"/root/projects/libra", page:"update-config", port:"8080"}
+];
+module.exports = config;
