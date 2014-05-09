59de4449ecede2761c1d6ab69bce7a961735d54b
Joshua Chavez <josh@findjosh.com>
2014-05-09 00:24:39 +0000

    added forever support

--- modules/startup/startup.js
+++ modules/startup/startup.js
@@ -8,43 +8,46 @@ function jsonConcat(o1, o2) {
     }
     return o1;
 }
-
+process.on('uncaughtException', function (err) {
+  console.log(err);
+})
 
 for (i = 0; i < config.startup.length; i++) {
     config.startup[i].callback = function (data) {
         if (data.status == "open") {
             if (data.type != undefined) {
                 if (data.type == "forever") {
-                    var forever = require('forever-monitor');
-                    //Like jquery.extend
-                    var childOptions = jsonConcat({
-                        max: 3,
-                        silent: true,
-                        options: []
-                    }, data);
-                    var child = new(forever.Monitor)(data.file, childOptions);
-                    child.on('exit', function () {
-                        console.log(childOptions.file + ' has exited after ' + childOptions.max + ' restarts');
-                    });
-                    child.on('stdout', function (data) {
-                        var buf = new Buffer(data);
-                        console.log(buf.toString("utf8"));
-                    });
-                    child.on('stderr', function (data) {
-                        var buf = new Buffer(data);
-                        console.log(buf.toString("utf8"));
-                    });
-                    child.on('start', function (process, data) {
-                        var buf = new Buffer(data);
-                        console.log(buf.toString("utf8"));
-                    });
-                    child.start();
+                        var forever = require('forever-monitor');
+                        //Like jquery.extend
+                        var childOptions = jsonConcat({
+                            max: 3,
+                            silent: true,
+                            options: []
+                        }, data);
+                        var child = new(forever.Monitor)(data.file, childOptions);
+                        child.on('exit', function () {
+                            console.log(childOptions.file + ' has exited after ' + childOptions.max + ' restarts');
+                        });
+                        child.on('stdout', function (data) {
+                            var buf = new Buffer(data);
+                            console.log(buf.toString("utf8"));
+                        });
+                        child.on('stderr', function (data) {
+                            var buf = new Buffer(data);
+                            console.log(buf.toString("utf8"));
+                        });
+                        child.on('start', function (process, data) {
+                            var buf = new Buffer(data);
+                            console.log(buf.toString("utf8"));
+                        });
 
+                        child.start();
+                }else{
+                    console.log("SERVICE ALREADY ON "+data.port)
                 }
 
             }
             else {
-                if (data.status == "open") {
                     var childProcess = require('child_process'),
                         ls;
                     ls = childProcess.exec(data.command, function (error, stdout, stderr) {
@@ -60,8 +63,9 @@ for (i = 0; i < config.startup.length; i++) {
                     ls.on('exit', function (code) {
                         console.log('Child process exited with exit code ' + code);
                     });
-                }
             }
+        }else{
+            console.log("SERVICE ALREADY ON "+data.port)
         }
     }
     libra.checkrunning.run(config.startup[i]);
