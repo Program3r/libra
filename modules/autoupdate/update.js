00f5da4b0423addd92b02e18b31cc148e5074033
Joshua Chavez <josh@findjosh.com>
2014-07-05 12:48:27 +0000

    remove debug and update test

--- modules/autoupdate/update.js
+++ modules/autoupdate/update.js
@@ -2,11 +2,9 @@ var git = require('gift');
 var repo = git('./');
 setInterval(function(){
     repo.status(function(err, data){
-        console.log(data)
         //Must be a live instance that needs update check.
         if(data.clean == true){
             repo.pull("origin", "auto-stage", function(err, status, one){
-                console.log(err, status, one)
             });
         }
     });
