7291b43d2d09dabd9d59e0c1712eca4a5ed0438d
Joshua Chavez <josh@findjosh.com>
2014-07-05 12:45:20 +0000

    debug testing

--- modules/autoupdate/update.js
+++ modules/autoupdate/update.js
@@ -2,12 +2,14 @@ var git = require('gift');
 var repo = git('./');
 setInterval(function(){
     repo.status(function(err, data){
+        console.log(data)
         //Must be a live instance that needs update check.
         if(data.clean == true){
             repo.pull("origin", "auto-update", function(err, status, one){
+                console.log(err, status, one)
             });
         }
     });
-}, 5000);
+}, 10000);
 
 //Minor Change Test 2
